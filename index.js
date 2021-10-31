04601dd7defd645a4e975f4577107d1db09d9481
benjamin <benjinoble@gmail.com>
2021-11-01 10:09:11 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function(event) {
+    return 'Hee Hee thats tickles'
+  });
 }
+
+function clickAlert() {
+    alert('Hee Hee thats tickles');
+}
+input.addEventListener('click' , clickAlert);
